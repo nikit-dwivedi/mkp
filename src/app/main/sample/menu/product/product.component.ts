c7c0946dbf5ad4804ab02391ecfd19612c4b9f58
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-05-18 18:13:59 +0530

    add non veg product

--- src/app/main/sample/menu/product/product.component.ts
+++ src/app/main/sample/menu/product/product.component.ts
@@ -18,14 +18,14 @@ export class ProductComponent implements OnInit {
   view: any;
   hasCustom: any;
   hasAddon: any;
-  switcher: boolean
-  isVeg: false
+  switcher: boolean;
+  isVeg: false;
   @Input() categoryData: any;
   productDetails: any;
   product: any;
   imageData: any;
   producyById: any;
-  constructor(private adminService: AdminService, private modalService: NgbModal, config: NgbModalConfig, private fb: FormBuilder, private toastr: ToastrService) { }
+  constructor(private adminService: AdminService, private modalService: NgbModal, config: NgbModalConfig, private fb: FormBuilder, private toastr: ToastrService) {}
 
   ngOnInit(): void {
     this.productByCatgeoryId();
@@ -36,7 +36,7 @@ export class ProductComponent implements OnInit {
       productPrice: new FormControl(""),
       productDesc: new FormControl(""),
       productImage: new FormControl(""),
-      isVeg: new FormControl("")
+      isVeg: new FormControl(""),
     });
     // edit poduct form
     this.editProductForm = this.fb.group({
@@ -61,7 +61,6 @@ export class ProductComponent implements OnInit {
     this.imageData = event.target.files[0];
   }
 
-
   // open add product Modal
   openAddProductModal(data: any) {
     this.modalService.open(data, {
@@ -70,22 +69,20 @@ export class ProductComponent implements OnInit {
       size: "lg",
     });
   }
-  
+
   addProductFormSubmit() {
     this.submitted = true;
     if (this.addProductForm.invalid) {
       return;
-    } else
-     {
-
+    } else {
       const formData = new FormData();
       formData.append("parentCategoryId", this.categoryData.categoryId);
       formData.append("productName", this.addProductForm.value.productName);
       formData.append("productPrice", this.addProductForm.value.productPrice);
       formData.append("productDesc", this.addProductForm.value.productDesc);
       formData.append("productImage", this.imageData);
-      formData.append("isVeg", this.addProductForm.value.isVeg);
-       
+      formData.append("isVeg", this.addProductForm.value.isVeg ?? false);
+
       this.adminService.addProduct(formData).subscribe((data: any) => {
         if (!data.status) {
           this.toastr.error(data.message, "failed");
@@ -100,14 +97,11 @@ export class ProductComponent implements OnInit {
 
   productByCatgeoryId() {
     this.adminService.getProductBycategory(this.categoryData?.categoryId).subscribe((data: any) => {
-     
       if (data.status) {
         this.productList = data.items;
-   }
+      }
     });
   }
-
-  
 
   // open edit product modal
   openEditProductModal(data: any, edit: any) {
@@ -152,9 +146,9 @@ export class ProductComponent implements OnInit {
   //  open view product modal
   openViewProductModal(data: any, view: any) {
     this.modalService.open(data, {
-      ariaLabelledBy: 'modal-basic-title',
-      size: 'lg',
-      windowClass: 'product-detail-modal'
+      ariaLabelledBy: "modal-basic-title",
+      size: "lg",
+      windowClass: "product-detail-modal",
     });
     this.view = view.productId;
 
@@ -167,33 +161,28 @@ export class ProductComponent implements OnInit {
     });
   }
 
-
-
-  openDeleteProductModal(data: any,product:any) {
+  openDeleteProductModal(data: any, product: any) {
     this.modalService.open(data, {
       centered: true,
       scrollable: true,
       size: "lg",
     });
     this.producyById = product.productId;
-    
   }
-  deleteProduct(){
-    const body ={
-      productId:this.producyById
-    }
-    this.adminService.deleteData(body).subscribe((data:any) => {
-      if(data.status){
-        this.toastr.success(data.message,"Success!");
+  deleteProduct() {
+    const body = {
+      productId: this.producyById,
+    };
+    this.adminService.deleteData(body).subscribe((data: any) => {
+      if (data.status) {
+        this.toastr.success(data.message, "Success!");
         this.productByCatgeoryId();
         this.modalService.dismissAll();
+      } else {
+        this.toastr.error(data.message, "error!");
       }
-      else{
-        this.toastr.error(data.message,"error!");
-      }
-    })
+    });
   }
-
 
   openViewAddonModal(data: any) {
     this.modalService.open(data, {
@@ -201,6 +190,5 @@ export class ProductComponent implements OnInit {
       scrollable: true,
       size: "lg",
     });
-    
   }
 }
